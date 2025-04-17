33e3a2de3703206ebf2ca334a8eb2b26cd78ff84
sofimicol <sofimicol3@gmail.com>
2025-04-17 19:37:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
-#include "arbol.h"
+#include "TAD_arbol.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
-	
+	str ptr;
+	int p;
 	tData A=NULL;
 	tData B=NULL;
-	printf("\nA continuacion, se crear· un conjunto A: ");
+	printf("\nA continuacion, se crear√° un conjunto A: ");
 	
 	A=cargaConjunto();
 	
 	printf("\nConjunto ingresado: \n{");
 	mostrarConjunto(A);
 	
-	printf("\nA continuacion, se crear· un conjunto B: ");
-
+	printf("\nA continuacion, se crear√° un conjunto B: ");
+	
 	B=cargaConjunto();
 	
 	printf("\nConjunto ingresado: \n{");
 	mostrarConjunto(B);
 	
 	printf("\nUnion de conjuntos: ");
-	mostrarConjunto(unionSet(A,B));
+	//mostrarConjunto(unionSet(A,B));
+	printf("\nIngrese elemento a buscar: ");
+	ptr=load();
+	p=inArbol(A,ptr);
+	if(p==0)printf("\nEl elemento pertenece al conjunto");
+	else printf("\nEl elemento no pertenece al conjunto");
 	
-	
-	
-	
-	liberarArbol(A);
+	eliminaConjunto(&A);
 	
 }
